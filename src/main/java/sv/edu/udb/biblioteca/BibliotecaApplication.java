1486030fa79beda7754e1bd79695acbd231379d8
B3atrriz <elisabetgonzalez2007@gmail.com>
2026-02-22 23:37:09 -0600

    Ultimos cambios

--- src/main/java/sv/edu/udb/biblioteca/BibliotecaApplication.java
+++ src/main/java/sv/edu/udb/biblioteca/BibliotecaApplication.java
@@ -1,4 +1,5 @@
 package sv.edu.udb.biblioteca;
+// Ruta: src/main/java/sv/edu/udb/biblioteca/BibliotecaApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ public class BibliotecaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BibliotecaApplication.class, args);
+        System.out.println(" La aplicación  ha arrancado con éxito");
     }
 
 }
